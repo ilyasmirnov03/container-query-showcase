843b4a522d6179668d2b65b19403397a7b72172a
Ilya Smirnov <ilua.smirnof@gmail.com>
2023-12-05 16:24:35 +0100

    wip

--- assets/js/responsive-card.js
+++ assets/js/responsive-card.js
@@ -1,32 +1,50 @@
 class ResponsiveCard extends HTMLElement {
 
-    constructor() {
+    canDelete;
+
+    /**
+     * @param {boolean} canDelete 
+     */
+    constructor(canDelete) {
         super();
+        this.canDelete = canDelete;
         this.classList.add('card-container');
     }
 
     connectedCallback() {
         this.render();
+        this.attachHandlers();
+    }
+
+    addNewComponent() {
+        this.appendChild(new ResponsiveCard(true));
     }
 
     render() {
+        if (this.canDelete) {
+            console.log('prepending');
+            this.insertAdjacentHTML('afterbegin', '<button class="card__delete">x</button>');
+        }
         this.innerHTML = `
         <article class="card">
-            <header>
-                <h2 class="card__title">Un mignon chaton et un kiwi</h2>
-                <img class="card__img" src="https://assets.codepen.io/9425/lilith.webp" alt="le chat mignon" width="600"
-                height="620">
-            </header>
-            <p class="card__desc">Affichage des différents état d'une "card" selon l'espace disponible au sein de son
-                parent. <br>
-                Réduisez la largeur du conteneur pour modifier les styles appliqués.</p>
-            <button class="card-btn">
-                <span class="btn-plus" aria-hidden="true">+</span>
-                <span class="btn-desc">ajouter au</span>
-                <span class="btn-content">panier</span>
+            <h2 class="card__title">Un mignon chaton et un kiwi</h2>
+            <img class="card__img" src="https://assets.codepen.io/9425/lilith.webp" alt="un chat mignon">
+            <p class="card__desc">
+                Affichage des différents état d'une "card" selon l'espace disponible au sein de son
+                parent. Réduisez la largeur du conteneur pour modifier les styles appliqués.
+            </p>
+            <button class="card__btn">
+                + ajouter nouveau composant
             </button>
         </article>
         `;
+        this.$btn = this.querySelector('.card__btn');
+        this.$delete = this.querySelector('.card__delete');
+    }
+
+    attachHandlers() {
+        this.$btn.addEventListener('click', this.addNewComponent.bind(this));
+        this.$delete?.addEventListener('click', this.remove);
     }
 }
 
